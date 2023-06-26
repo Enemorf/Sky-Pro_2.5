835c51f7fb0e712d4a553bef8cb5ad06dde54d36
Victor <svkolychev@yandex.ru>
2023-06-26 16:14:48 +0300

    Выполнено ДЗ №7

--- src/main/java/part5/SkyProHomeworks/homework_5/EmployeeService.java
+++ src/main/java/part5/SkyProHomeworks/homework_5/EmployeeService.java
@@ -5,52 +5,51 @@ import part5.SkyProHomeworks.homework_5.exeptions.EmployeeAlreadyAddedException;
 import part5.SkyProHomeworks.homework_5.exeptions.EmployeeNotFoundException;
 import part5.SkyProHomeworks.homework_5.exeptions.EmployeeStorageIsFullException;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmployeeService
 {
-    private final int maxEmployee = 10;
-    private List<Employee> employeeList = new ArrayList<>();
+    private Map<String,Employee> employeeMap = new HashMap<>();
+    String checkName;
 
     public Employee addEmployee (String firstName, String lastName)
     {
-        if(employeeList.size() == maxEmployee)
-            throw new EmployeeStorageIsFullException();
-        Employee employee = new Employee(firstName,lastName);
+        checkName = firstName+lastName;
 
-        if(employeeList.contains(employee))
+        if(employeeMap.containsKey(checkName))
             throw new EmployeeAlreadyAddedException();
 
-        employeeList.add(employee);
+        Employee employee = new Employee(firstName,lastName);
+
+        employeeMap.put(checkName,employee);
+
         return employee;
     }
 
     public Employee deleteEmployee (String firstName, String lastName)
     {
-        Employee employee = new Employee(firstName, lastName);
+        checkName = firstName+lastName;
 
-        if(!employeeList.contains(employee))
+        if(!employeeMap.containsKey(checkName))
             throw new EmployeeNotFoundException();
 
-        employeeList.remove(employee);
-        return employee;
+        return employeeMap.remove(checkName);
     }
 
     public Employee searchEmployee (String firstName, String lastName)
     {
-        Employee employee = new Employee(firstName, lastName);
+        checkName = firstName+lastName;
 
-        if(!employeeList.contains(employee))
+        if(!employeeMap.containsKey(checkName))
             throw new EmployeeNotFoundException();
-        return employee;
+
+        return employeeMap.get(checkName);
     }
 
     public String printAll ()
     {
-        return employeeList.toString();
+        return employeeMap.values().toString();
     }
 
 }
